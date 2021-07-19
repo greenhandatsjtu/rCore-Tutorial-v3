d7cdb162dbf44c1f0223dfcf97ff363fb58b6929
greenhandatsjtu <i-hengke.sun@clobotics.com>
2021-07-19 19:15:42 +0800

    merge lab1

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,6 +4,8 @@
 #![feature(llvm_asm)]
 #![feature(panic_info_message)]
 
+use log::*;
+
 #[macro_use]
 mod console;
 mod lang_items;
@@ -11,6 +13,7 @@ mod sbi;
 mod syscall;
 mod trap;
 mod batch;
+mod logging;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -28,6 +31,8 @@ fn clear_bss() {
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
+    logging::init();
+    info!("Logger inited.");
     println!("[kernel] Hello, world!");
     trap::init();
     batch::init();
