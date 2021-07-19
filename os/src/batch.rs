fd7102b091ca444c7ffa6da6ea14debe9871d065
greenhandatsjtu <i-hengke.sun@clobotics.com>
2021-07-19 20:31:26 +0800

    finish lab2

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -2,8 +2,8 @@ use core::cell::RefCell;
 use lazy_static::*;
 use crate::trap::TrapContext;
 
-const USER_STACK_SIZE: usize = 4096 * 2;
-const KERNEL_STACK_SIZE: usize = 4096 * 2;
+const USER_STACK_SIZE: usize = 4096;
+const KERNEL_STACK_SIZE: usize = 4096;
 const MAX_APP_NUM: usize = 16;
 const APP_BASE_ADDRESS: usize = 0x80400000;
 const APP_SIZE_LIMIT: usize = 0x20000;
--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -3,13 +3,22 @@ const FD_STDOUT: usize = 1;
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
+            extern "C" {
+                fn skernel();
+                fn ekernel();
+            }
+            if (buf as isize) < (skernel as isize) || (buf as isize + len as isize) > (ekernel as isize) {
+                println!("Illegal write!");
+                return -1;
+            }
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
             let str = core::str::from_utf8(slice).unwrap();
             print!("{}", str);
             len as isize
-        },
+        }
         _ => {
-            panic!("Unsupported fd in sys_write!");
+            println!("Unsupported fd in sys_write!");
+            -1
         }
     }
 }
